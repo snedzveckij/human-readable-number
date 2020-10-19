8b65d7588a4114f131462709c5b720ddb7f26425
Stanislav Nedzvetskiy <snedzveckij@gmail.com>
2020-10-19 17:36:25 +0300

    feat: implemented function toReadable using npm num-words

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function toReadable (number) {
-  
+    const numWords = require('num-words');
+    const amountInWords = numWords(number);
+    return amountInWords.replace(" and ", " ");
 }
